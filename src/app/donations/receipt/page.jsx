c36816ec13247e02cd5d079e4399489011980a99
NeetuSharma3012 <riyasharma16602@gmail.com>
2025-06-15 17:53:33 +0530

    receipt edited

--- src/app/donations/receipt/page.jsx
+++ src/app/donations/receipt/page.jsx
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic'; // This page should always be re-rendered on each request
 import { useSearchParams } from 'next/navigation';
 import React from 'react';
 
