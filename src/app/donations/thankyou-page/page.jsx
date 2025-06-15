91f285331f9603e880cf4e20b05caa49d380b5c4
NeetuSharma3012 <riyasharma16602@gmail.com>
2025-06-15 18:01:47 +0530

    thank you compt

--- src/app/donations/thankyou-page/page.jsx
+++ src/app/donations/thankyou-page/page.jsx
@@ -1,29 +1,13 @@
 'use client';
-import { useSearchParams } from 'next/navigation';
-import React from 'react';
+
+import React, { Suspense } from 'react';
+import ThankYouContent from '@/app/donations/ThankyouComponent';
 
 const ThankYouPage = () => {
-  const searchParams = useSearchParams();
-  const transactionId = searchParams.get('paymentId');
-  const amount = searchParams.get('amount');
-
   return (
-    <div className="text-center py-16">
-      <h1 className="text-4xl font-bold text-lime-600">Thank you for your donation!</h1>
-      <p className="mt-4 text-gray-700">Your generosity makes a real difference ❤️</p>
-
-      <div className="mt-8 text-lg text-gray-600">
-        <p><strong>Transaction ID:</strong> {transactionId}</p>
-        <p><strong>Amount:</strong> ₹{amount}</p>
-      </div>
-
-      <a
-        href={`/donations/receipt?transactionId=${transactionId}&amount=${amount}`}
-        className="mt-6 inline-block px-5 py-3 bg-lime-400 text-black rounded-lg hover:bg-lime-500 transition"
-      >
-        Download your receipt
-      </a>
-    </div>
+    <Suspense fallback={<div className="text-center py-16">Loading...</div>}>
+      <ThankYouContent/>
+    </Suspense>
   );
 };
 
